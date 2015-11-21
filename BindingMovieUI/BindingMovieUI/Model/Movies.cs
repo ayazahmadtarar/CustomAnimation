e07d72ae101b2adca9ff19dd42a495d0ce48f332
ayazahmadtarar <ayazahmad@ymail.com>
2015-11-21 22:25:53 +0500

    first

---
+++ BindingMovieUI/BindingMovieUI/Model/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingMovieUI.Model
+{
+   public class Movies
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+        public string CoverImage { get; set; }
+
+    }
+}
